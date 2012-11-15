df0ce1f0e72c4c611fc5d0047b5661adb9f1085b
Rasathus <chris@fane.cc>
2012-11-15 23:05:32 +0000

    Update examples/ws2801.py

--- examples/ws2801.py
+++ examples/ws2801.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     led_chain.set_ic(ic_id=24, rgb_value=[255,0,255], lumi=50)
     led_chain.write()
     time.sleep(2)
-    led_chain.set_ic(ic_id=24, rgb_value=[0,0,0], lumi=50)
+    led_chain.set_ic(ic_id=24, rgb_value=[0,0,0])
     led_chain.write()
     time.sleep(0.5)
     
