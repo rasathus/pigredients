d28c6e7ca7446ac280a5609e2ece459ff30355de
Chris Fane <chris@fane.cc>
2013-01-20 13:20:31 +0000

    Implemented support for the display connectivity provided by GPIO3.  Implementation allows the writing of an ascii aubset to the screen, including support for cursor positions, allow a partial update of the display.  Also implements bar graphs, which can be used to display percentages onscreen.

---
+++ examples/pixi200-display.py
@@ -0,0 +1,58 @@
+from time import sleep
+from random import randint
+
+from pigredients.boards import pixi200 as pixi200
+
+
+pixi = pixi200.PIXI200(i2c_bus=1, debug=False)
+_display_write_register = 0x38 # 16bits 
+
+
+try:
+	"""
+	x_cord=0
+	y_cord=0
+	while True:
+		pixi.display_clear()
+		pixi.display_write("PiXi-200",x=x_cord, y=y_cord )
+		sleep(0.25)
+		x_cord = randint(0,39)
+		y_cord = randint(0,1)
+		print "x : %d y : %d" % (x_cord, y_cord)
+	"""
+	# write out our axis labels.
+	pixi.display_write("X", x=9, y=0)
+	pixi.display_write("Y", x=30, y=0)
+	pixi.display_write("Z", x=9, y=1)
+	perc_const = (100 / 32.0)
+
+	while True:
+		current_state = pixi.accel.get_value()
+		#pixi.display_clear()
+
+		if current_state['x'] > 32:
+			pixi.display_bar_graph(x=0, y=0, length=9, percentage=int(perc_const * (32 - (current_state['x'] - 32))), reverse=True)
+			pixi.display_bar_graph(x=10, y=0, length=9, percentage=0, reverse=False)
+		else:
+			pixi.display_bar_graph(x=0, y=0, length=9, percentage=0, reverse=True)
+			pixi.display_bar_graph(x=10, y=0, length=9, percentage=int(perc_const * (current_state['x'])), reverse=False)
+
+		if current_state['y'] > 32:
+			pixi.display_bar_graph(x=21, y=0, length=9, percentage=int(perc_const * (32 - (current_state['y'] - 32))), reverse=True)
+			pixi.display_bar_graph(x=31, y=0, length=9, percentage=0, reverse=False)
+		else:
+			pixi.display_bar_graph(x=21, y=0, length=9, percentage=0, reverse=True)
+			pixi.display_bar_graph(x=31, y=0, length=9, percentage=int(perc_const * (current_state['y'])), reverse=False)
+
+		if current_state['z'] > 32:
+			pixi.display_bar_graph(x=0, y=1, length=9, percentage=int(perc_const * (32 - (current_state['z'] - 32))), reverse=True)
+			pixi.display_bar_graph(x=10, y=1, length=9, percentage=0, reverse=False)
+		else:
+			pixi.display_bar_graph(x=0, y=1, length=9, percentage=0, reverse=True)
+			pixi.display_bar_graph(x=10, y=1, length=9, percentage=int(perc_const * (current_state['z'])), reverse=False)
+
+		sleep(0.1)
+	
+except (KeyboardInterrupt, SystemExit):
+	pixi.display_clear()
+
--- pigredients/boards/pixi200.py
+++ pigredients/boards/pixi200.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 import sys
 import smbus
+import time
 
 import spidev
 from Adafruit_I2C import Adafruit_I2C
@@ -14,9 +16,15 @@ _leds_config_register = 0x31 # 4bits, Always set to 0000 , we'll worry about oth
 
 _switch_in_register = 0x32 # 8bits, 2bits per switch, first bit current state, second bit 1 if state change since last read, automatically resets on read.
 
+_display_write_register = 0x38 # 16bits 
+
 _valid_led_ids = ['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8']
 _valid_led_ids.reverse()
 _valid_led_state = {'on' : '11', 'off' : '00', 'slow' : '01', 'fast' : '10'}
+_valid_display_brightness = {100 : 0, 75 : 1, 50 : 2, 25 : 3}
+
+_max_display_xcord = 39
+_max_display_ycord = 1
 
 class response_object(object):
     def __init__(self):
@@ -30,7 +38,7 @@ class response_object(object):
         return str(self.__dict__)
 
 class PIXI200(object):
-    def __init__(self, spi_address_hardware=0, spi_address_output=0, i2c_bus=0, i2c_address=_pixi_i2c_address, debug=False):
+    def __init__(self, spi_address_hardware=0, spi_address_output=0, i2c_bus=0, i2c_address=_pixi_i2c_address, use_display=True, debug=False):
         self.debug = debug
         # setup spi comms
         self.spi = spidev.SpiDev()
@@ -49,14 +57,20 @@ class PIXI200(object):
             self._led_state[led] = 00        
         # set our led mode to 0000, this may be the default.
         self.set_led_mode(0000)
+        if use_display:
+            # initialised our display outputs on GPIO3
+            self._init_display()
 
     def _spi_write(self, register_address, data):
-        if data is not list or len(data) != 2:
-            raise Exception("Spi write data should be in the form of list containing 2 bytes.")
+        #if type(data) is not list or len(data) != 2:
+        #    raise Exception("Spi write data should be in the form of list containing 2 bytes.")
         response = response_object()
         addresses_list = [register_address, 0x40]
         response.response = self.spi.xfer2(addresses_list + data)
-        if response.response[0] == 0x30:
+        if self.debug:
+            print "wrote : %s to register :%s" % (data, hex(register_address))
+
+        if response.response[0] == register_address:
             response.success = True
         return response
     
@@ -104,9 +118,9 @@ class PIXI200(object):
 
     def set_led(self, led_id, state):
         if led_id not in _valid_led_ids:
-            raise Exception("Invalid led id given, valid led ids are : %s " % _valid_led_ids)
+            raise Exception("Invalid led id given, valid led ids are : %s " % _valid_led_ids.keys())
         if state not in _valid_led_state:
-            raise Exception("Invalid led state given, valid led states are : %s " % _valid_led_state)
+            raise Exception("Invalid led state given, valid led states are : %s " % _valid_led_state.keys())
         self._led_state[led_id] = _valid_led_state[state]
 
     def set_on(self):
@@ -133,4 +147,111 @@ class PIXI200(object):
         #print "data list contains : %s" % data_list
         response = self.spi.xfer2(data_list)
         #print "led set got the response : %s" % response
+
+
+    def display_clear(self):
+        response = self._spi_write(0x38, [0,1])
+
+    def _init_display(self):
+        # Setup our GPIO3 modes for display write
+        response = self._spi_write(0x2c, [0x00,0x02])
+        response = self._spi_write(0x2d, [0x00,0x02])
+        self.display_clear()
+
+    def _display_brightness(self, brightness):
+        # UNFINISHED !!!!
+        if brightness not in _valid_display_brightness:
+            raise Exception("Invalid brightness given, valid states are : %s " % _valid_display_brightness.keys())
+
+        response = self._spi_write(0x38, [0x02,_valid_display_brightness[brightness]])
+        print "Sent brightness command to 0x38 : %s " % response
+
+    def display_state(self, state):
+        if state not in ['on','off']:
+            raise Exception("Invalid display state given, valid states are 'on' or 'off'")
+
+        if state == 'off':
+            response = self._spi_write(0x38, [0,8])
+        else:
+            response = self._spi_write(0x38, [0,12])
         
+        
+    def _display_write(self, data):
+        if type(data) is list:
+            for element in data:
+                response = self.spi.xfer2([_display_write_register,0x40, 0x02, element])            
+        else:
+            for char in data:
+                data_list = [_display_write_register,0x40]
+                data_list.append(0x02) # set our rs bit
+                try:
+                    data_list.append(ord(char))
+                except ValueError:
+                    # unknown char, using a placeholder instead.
+                    data_list.append(0x3f) # insert a question mark instead.
+                except TypeError:
+                    # ord throws a type error if given something other than a string. if so check its an int and pass it along anyway, to allow us to send hex codes.
+                    try:
+                        data_list.append(int(char)) # send the value unprocessed
+                    except ValueError:
+                        # unknown char, using a placeholder instead.
+                        data_list.append(0x3f) # insert a question mark instead.
+
+                response = self.spi.xfer2(data_list)
+        return response
+
+    def display_write(self, data, x=0, y=0):
+        # Setup our GPIO3 modes for display write
+        if x > _max_display_xcord or y > _max_display_ycord :
+            raise Exception("Invalid display coordinate given, x coordinates should be less than or equal to %d and y coordinates less than or equal to %d" % (_max_display_xcord, _max_display_ycord))
+        # send our x,y curser start addresses
+        cursor_pos = 128 + (y << 6) + x
+        self._spi_write(0x38,[0x00, cursor_pos])
+        # send our data to the display
+        self._display_write(data)
+
+    def display_bar_graph(self, x, y , length, percentage, reverse=False):
+        try:
+            if int(percentage) > 100 :
+                raise ValueError
+        except ValueError:
+            raise Exception("Invalid percentage given, valid percentages are integers between 0 and 100")
+
+        data_list = []
+        filled_elements = int((((5.0 * length) / 100 ) * percentage))
+        for element in range(length):
+            if filled_elements >= 5:
+                data_list.append(0x14)
+                filled_elements = filled_elements - 5
+            elif filled_elements == 4:
+                if reverse:
+                    data_list.append(0x15)    
+                else:
+                    data_list.append(0x13)
+                filled_elements = filled_elements - 4
+            elif filled_elements == 3:
+                if reverse:
+                    data_list.append(0x16)    
+                else:
+                    data_list.append(0x12)
+                filled_elements = filled_elements - 3
+            elif filled_elements == 2:
+                if reverse:
+                    data_list.append(0x17)    
+                else:
+                    data_list.append(0x11)
+                filled_elements = filled_elements - 2
+            elif filled_elements == 1:
+                if reverse:
+                    data_list.append(0x18)    
+                else:
+                    data_list.append(0x10)
+                filled_elements = filled_elements - 1
+            else:
+                data_list.append(0x20)
+                filled_elements = filled_elements - 0
+
+        if reverse:
+            data_list.reverse()
+        self.display_write(data_list, x=x, y=y)
+
